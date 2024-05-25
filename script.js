4f98755045edc4d6b36186f3a6403e06ae0c0911
Shang-Min <happymin0318@gmail.com>
2024-05-25 10:12:07 +0800

    更改檔名

--- script.js
+++ script.js
@@ -4,81 +4,108 @@ var days = [
     title: "JavaScript Drum Kit",
     desc: "創建一個可以透過按鍵發聲的爵士鼓應用程式。",
     blog: "",
-    demo: "",
+    demo: "https://hsu0201.github.io/JavaScript30/01-JavaScriptDrumKit/hsuJS01.html",
     github: "",
     gifYn: "Y",
+    image:
+      "https://github.com/HSU0201/JavaScript30/blob/master/img/01.png?raw=true",
+    gif: "https://github.com/HSU0201/JavaScript30/blob/master/img/01.gif?raw=true",
   },
   {
     no: "Day02",
     title: "JS and CSS Clock",
     desc: "開發一個實時顯示時間的時鐘應用程式。",
     blog: "",
-    demo: "",
+    demo: "https://hsu0201.github.io/JavaScript30/01-JavaScriptDrumKit/hsuJS01.html",
     github: "",
     gifYn: "Y",
+    image:
+      "https://github.com/HSU0201/JavaScript30/blob/master/img/02.png?raw=true",
+    gif: "https://github.com/HSU0201/JavaScript30/blob/master/img/02.gif?raw=true",
   },
   {
     no: "Day03",
     title: "CSS Variables",
     desc: "設計一個可調整圖片邊框、模糊度和黑白效果的濾鏡應用。",
     blog: "",
-    demo: "",
+    demo: "https://hsu0201.github.io/JavaScript30/01-JavaScriptDrumKit/hsuJS01.html",
     github: "",
     gifYn: "Y",
+    image:
+      "https://github.com/HSU0201/JavaScript30/blob/master/img/03.png?raw=true",
+    gif: "https://github.com/HSU0201/JavaScript30/blob/master/img/03.gif?raw=true",
   },
   {
     no: "Day04",
     title: "Array Cardio Day 1",
     desc: "展示 filter、map、sort、reduce、includes 和 split 等方法的實際應用。",
     blog: "",
-    demo: "",
+    demo: "https://hsu0201.github.io/JavaScript30/01-JavaScriptDrumKit/hsuJS01.html",
     github: "",
     gifYn: "N",
+    image:
+      "https://github.com/HSU0201/JavaScript30/blob/master/img/04.png?raw=true",
+    gif: "",
   },
   {
     no: "Day05",
     title: "Flex Panel Gallery",
     desc: "開發一個具有動畫展開效果的圖片展示應用。",
     blog: "",
-    demo: "",
+    demo: "https://hsu0201.github.io/JavaScript30/01-JavaScriptDrumKit/hsuJS01.html",
     github: "",
     gifYn: "Y",
+    image:
+      "https://github.com/HSU0201/JavaScript30/blob/master/img/05.png?raw=true",
+    gif: "https://github.com/HSU0201/JavaScript30/blob/master/img/05.gif?raw=true",
   },
   {
     no: "Day06",
     title: "Type Ahead",
     desc: "創建一個即時顯示搜尋結果的輸入框應用程式。",
     blog: "",
-    demo: "",
+    demo: "https://hsu0201.github.io/JavaScript30/01-JavaScriptDrumKit/hsuJS01.html",
     github: "",
     gifYn: "Y",
+    image:
+      "https://github.com/HSU0201/JavaScript30/blob/master/img/06.png?raw=true",
+    gif: "https://github.com/HSU0201/JavaScript30/blob/master/img/06.gif?raw=true",
   },
   {
     no: "Day07",
     title: "Array Cardio Day 2",
     desc: "展示 some、every、find、findIndex 和 slice 方法的應用案例。",
     blog: "",
-    demo: "",
+    demo: "https://hsu0201.github.io/JavaScript30/01-JavaScriptDrumKit/hsuJS01.html",
     github: "",
     gifYn: "N",
+    image:
+      "https://github.com/HSU0201/JavaScript30/blob/master/img/07.png?raw=true",
+    gif: "",
   },
   {
     no: "Day08",
     title: "Fun with HTML5 Canvas",
     desc: "設計一個使用 HTML5 Canvas 的繪圖應用程式。",
     blog: "",
-    demo: "",
+    demo: "https://hsu0201.github.io/JavaScript30/01-JavaScriptDrumKit/hsuJS01.html",
     github: "",
     gifYn: "Y",
+    image:
+      "https://github.com/HSU0201/JavaScript30/blob/master/img/08.png?raw=true",
+    gif: "",
   },
   {
     no: "Day09",
     title: "Dev Tools Domination",
     desc: "介紹 Chrome 開發工具中的 DOM 斷點及各種 console 格式。",
     blog: "",
-    demo: "",
+    demo: "https://hsu0201.github.io/JavaScript30/01-JavaScriptDrumKit/hsuJS01.html",
     github: "",
     gifYn: "N",
+    image:
+      "https://github.com/HSU0201/JavaScript30/blob/master/img/09.png?raw=true",
+    gif: "",
   },
   {
     no: "Day10",
@@ -88,186 +115,249 @@ var days = [
     demo: "",
     github: "",
     gifYn: "Y",
+    image:
+      "https://github.com/HSU0201/JavaScript30/blob/master/img/10.png?raw=true",
+    gif: "https://github.com/HSU0201/JavaScript30/blob/master/img/10.gif?raw=true",
   },
   {
     no: "Day11",
     title: "Custom Video Player",
     desc: "使用 HTML5 video 標籤創建一個自訂影片播放器。",
     blog: "",
-    demo: "",
+    demo: "https://hsu0201.github.io/JavaScript30/01-JavaScriptDrumKit/hsuJS01.html",
     github: "",
     gifYn: "Y",
+    image:
+      "https://github.com/HSU0201/JavaScript30/blob/master/img/11.png?raw=true",
+    gif: "https://github.com/HSU0201/JavaScript30/blob/master/img/11.gif?raw=true",
   },
   {
     no: "Day12",
     title: "Key Sequence Detection",
     desc: "開發一個偵測連續輸入特定內容的應用程式。",
     blog: "",
-    demo: "",
+    demo: "https://hsu0201.github.io/JavaScript30/01-JavaScriptDrumKit/hsuJS01.html",
     github: "",
     gifYn: "Y",
+    image:
+      "https://github.com/HSU0201/JavaScript30/blob/master/img/12.png?raw=true",
+    gif: "https://github.com/HSU0201/JavaScript30/blob/master/img/12.gif?raw=true",
   },
   {
     no: "Day13",
     title: "Slide in on Scroll",
     desc: "設計一個當畫面滾動到特定位置時顯示圖片的效果。",
     blog: "",
-    demo: "",
+    demo: "https://hsu0201.github.io/JavaScript30/01-JavaScriptDrumKit/hsuJS01.html",
     github: "",
     gifYn: "Y",
+    image:
+      "https://github.com/HSU0201/JavaScript30/blob/master/img/13.png?raw=true",
+    gif: "https://github.com/HSU0201/JavaScript30/blob/master/img/13.gif?raw=true",
   },
   {
     no: "Day14",
     title: "JavaScript References VS Copying",
     desc: "介紹陣列和物件的引用與複製的區別。",
     blog: "",
-    demo: "",
+    demo: "https://hsu0201.github.io/JavaScript30/01-JavaScriptDrumKit/hsuJS01.html",
     github: "",
     gifYn: "N",
+    image:
+      "https://github.com/HSU0201/JavaScript30/blob/master/img/14.png?raw=true",
+    gif: "",
   },
   {
     no: "Day15",
     title: "LocalStorage",
     desc: "開發一個利用 localStorage 進行菜單增刪操作的應用程式。",
     blog: "",
-    demo: "",
+    demo: "https://hsu0201.github.io/JavaScript30/01-JavaScriptDrumKit/hsuJS01.html",
     github: "",
     gifYn: "Y",
+    image:
+      "https://github.com/HSU0201/JavaScript30/blob/master/img/15.png?raw=true",
+    gif: "https://github.com/HSU0201/JavaScript30/blob/master/img/15.gif?raw=true",
   },
   {
     no: "Day16",
     title: "Mouse Move Shadow",
     desc: "設計一個根據滑鼠移動改變文字陰影的效果。",
     blog: "",
-    demo: "",
+    demo: "https://hsu0201.github.io/JavaScript30/01-JavaScriptDrumKit/hsuJS01.html",
     github: "",
     gifYn: "Y",
+    image:
+      "https://github.com/HSU0201/JavaScript30/blob/master/img/16.png?raw=true",
+    gif: "https://github.com/HSU0201/JavaScript30/blob/master/img/16.gif?raw=true",
   },
   {
     no: "Day17",
     title: "Sort Without Articles",
     desc: "介紹如何在排序時排除特定字詞。",
     blog: "",
-    demo: "",
+    demo: "https://hsu0201.github.io/JavaScript30/01-JavaScriptDrumKit/hsuJS01.html",
     github: "",
     gifYn: "N",
+    image:
+      "https://github.com/HSU0201/JavaScript30/blob/master/img/17.png?raw=true",
+    gif: "",
   },
   {
     no: "Day18",
     title: "Adding Up Times with Reduce",
     desc: "使用 Array 的 map() 和 reduce() 方法計算播放清單的總時長。",
     blog: "",
-    demo: "",
+    demo: "https://hsu0201.github.io/JavaScript30/01-JavaScriptDrumKit/hsuJS01.html",
     github: "",
     gifYn: "N",
+    image:
+      "https://github.com/HSU0201/JavaScript30/blob/master/img/18.png?raw=true",
+    gif: "",
   },
   {
     no: "Day19",
     title: "Webcam Fun",
     desc: "利用 getUserMedia 和 canvas 來實現影像濾鏡效果。",
     blog: "",
-    demo: "",
+    demo: "https://hsu0201.github.io/JavaScript30/01-JavaScriptDrumKit/hsuJS01.html",
     github: "",
     gifYn: "Y",
+    image:
+      "https://github.com/HSU0201/JavaScript30/blob/master/img/19.png?raw=true",
+    gif: "https://github.com/HSU0201/JavaScript30/blob/master/img/19.gif?raw=true",
   },
   {
     no: "Day20",
     title: "Speech Detection",
     desc: "開發一個將語音輸入轉換為文字的工具。",
     blog: "",
-    demo: "",
+    demo: "https://hsu0201.github.io/JavaScript30/01-JavaScriptDrumKit/hsuJS01.html",
     github: "",
     gifYn: "Y",
+    image:
+      "https://github.com/HSU0201/JavaScript30/blob/master/img/20.png?raw=true",
+    gif: "https://github.com/HSU0201/JavaScript30/blob/master/img/20.gif?raw=true",
   },
   {
     no: "Day21",
     title: "Geolocation",
     desc: "創建一個顯示當前地理位置和速度的應用程式。",
     blog: "",
-    demo: "",
+    demo: "https://hsu0201.github.io/JavaScript30/01-JavaScriptDrumKit/hsuJS01.html",
     github: "",
     gifYn: "N",
+    image:
+      "https://github.com/HSU0201/JavaScript30/blob/master/img/21.png?raw=true",
+    gif: "",
   },
   {
     no: "Day22",
     title: "Follow Along Link Highlighter",
     desc: "設計一個會隨滑鼠移動的高亮效果。",
     blog: "",
-    demo: "",
+    demo: "https://hsu0201.github.io/JavaScript30/01-JavaScriptDrumKit/hsuJS01.html",
     github: "",
     gifYn: "Y",
+    image:
+      "https://github.com/HSU0201/JavaScript30/blob/master/img/22.png?raw=true",
+    gif: "https://github.com/HSU0201/JavaScript30/blob/master/img/22.gif?raw=true",
   },
   {
     no: "Day23",
     title: "Speech Synthesis",
     desc: "開發一個將文字轉換為語音的工具。",
     blog: "",
-    demo: "",
+    demo: "https://hsu0201.github.io/JavaScript30/01-JavaScriptDrumKit/hsuJS01.html",
     github: "",
     gifYn: "Y",
+    image:
+      "https://github.com/HSU0201/JavaScript30/blob/master/img/23.png?raw=true",
+    gif: "https://github.com/HSU0201/JavaScript30/blob/master/img/23.gif?raw=true",
   },
   {
     no: "Day24",
     title: "Sticky Nav",
     desc: "設計一個根據捲動位置變化樣式的固定導航欄。",
     blog: "",
-    demo: "",
+    demo: "https://hsu0201.github.io/JavaScript30/01-JavaScriptDrumKit/hsuJS01.html",
     github: "",
     gifYn: "Y",
+    image:
+      "https://github.com/HSU0201/JavaScript30/blob/master/img/24.png?raw=true",
+    gif: "https://github.com/HSU0201/JavaScript30/blob/master/img/24.gif?raw=true",
   },
   {
     no: "Day25",
     title: "Event Capture, Propagation, Bubbling and Once",
     desc: "解析 addEventListener 方法中的捕捉、傳遞及單次執行功能。",
     blog: "",
-    demo: "",
+    demo: "https://hsu0201.github.io/JavaScript30/01-JavaScriptDrumKit/hsuJS01.html",
     github: "",
     gifYn: "N",
+    image:
+      "https://github.com/HSU0201/JavaScript30/blob/master/img/25.png?raw=true",
+    gif: "",
   },
   {
     no: "Day26",
     title: "Stripe Follow Along Nav",
     desc: "開發一個隨著滑鼠移動展開的導航選單效果。",
     blog: "",
-    demo: "",
+    demo: "https://hsu0201.github.io/JavaScript30/01-JavaScriptDrumKit/hsuJS01.html",
     github: "",
     gifYn: "Y",
+    image:
+      "https://github.com/HSU0201/JavaScript30/blob/master/img/26.png?raw=true",
+    gif: "https://github.com/HSU0201/JavaScript30/blob/master/img/26.gif?raw=true",
   },
   {
     no: "Day27",
     title: "Click and Drag",
     desc: "創建一個可以拖曳移動的水平捲動應用。",
     blog: "",
-    demo: "",
+    demo: "https://hsu0201.github.io/JavaScript30/01-JavaScriptDrumKit/hsuJS01.html",
     github: "",
     gifYn: "Y",
+    image:
+      "https://github.com/HSU0201/JavaScript30/blob/master/img/27.png?raw=true",
+    gif: "https://github.com/HSU0201/JavaScript30/blob/master/img/27.gif?raw=true",
   },
   {
     no: "Day28",
     title: "Video Speed Controller",
     desc: "設計一個可調節影片播放速度的控件。",
     blog: "",
-    demo: "",
+    demo: "https://hsu0201.github.io/JavaScript30/01-JavaScriptDrumKit/hsuJS01.html",
     github: "",
     gifYn: "Y",
+    image:
+      "https://github.com/HSU0201/JavaScript30/blob/master/img/28.png?raw=true",
+    gif: "https://github.com/HSU0201/JavaScript30/blob/master/img/28.gif?raw=true",
   },
   {
     no: "Day29",
     title: "Countdown Timer",
     desc: "開發一個自定義時間的倒數計時器。",
     blog: "",
-    demo: "",
+    demo: "https://hsu0201.github.io/JavaScript30/01-JavaScriptDrumKit/hsuJS01.html",
     github: "",
     gifYn: "Y",
+    image:
+      "https://github.com/HSU0201/JavaScript30/blob/master/img/29.png?raw=true",
+    gif: "https://github.com/HSU0201/JavaScript30/blob/master/img/29.gif?raw=true",
   },
   {
     no: "Day30",
     title: "Whack A Mole",
     desc: "創建一個打地鼠的小遊戲。",
     blog: "",
-    demo: "",
+    demo: "https://hsu0201.github.io/JavaScript30/01-JavaScriptDrumKit/hsuJS01.html",
     github: "",
     gifYn: "Y",
+    image:
+      "https://github.com/HSU0201/JavaScript30/blob/master/img/30.png?raw=true",
+    gif: "https://github.com/HSU0201/JavaScript30/blob/master/img/30.gif?raw=true",
   },
 ];
 
@@ -275,15 +365,26 @@ function createView() {
   let main = document.querySelector(".main");
   let view = "";
   days.forEach((day) => {
-    view += `<div class="card">
+    view += `
+      <div class="card" 
+        data-title="${day.title}" 
+        data-desc="${day.desc}" 
+        data-demo="${day.demo}" 
+        data-github="${day.github}"  
+        data-img="${day.image}" 
+        data-gif="${day.gif}"
+      >
                 <div class="tag">
                   <div class="tag-day">${day.no}</div>
                 </div>
-                <img
-                class="img"
-                src="https://via.placeholder.com/300"
-                alt="${day.no}"
-                />
+                <div class="img-box">
+                  <img
+                  class="img"
+                  src="${day.image}"
+                  alt="${day.no}"
+                  />
+                </div>
+                
                 <div class="card-content">
                   <h3 class="card-title">${day.title}</h3>
                   <p class="card-text">
@@ -294,8 +395,61 @@ function createView() {
                   <a class="card-btn" href="${day.demo}" target="_blank">DEMO</a>
                   <a class="card-btn" href="${day.github}" target="_blank">GitHub</a>
                 </div>
-            </div>`;
+      </div>`;
   });
   main.innerHTML = view;
+  const cards = Array.from(document.querySelectorAll(".card"));
+  cards.forEach((card) => {
+    card.addEventListener("mouseenter", changeImageType);
+    card.addEventListener("mouseleave", changeImageType);
+    card.addEventListener("click", enlargeCard);
+  });
 }
+
+function changeImageType(e) {
+  const card = e.currentTarget;
+  const img = card.querySelector(".img");
+  const gifSrc = card.dataset.gif;
+  const imgSrc = card.dataset.img;
+  if (e.type === "mouseenter" && gifSrc) {
+    img.src = gifSrc;
+  } else if (e.type === "mouseleave") {
+    img.src = imgSrc;
+  }
+}
+
+function enlargeCard(e) {
+  const card = e.currentTarget;
+  const title = card.dataset.title;
+  const desc = card.dataset.desc;
+  const demo = card.dataset.demo;
+  const github = card.dataset.github;
+  const imgSrc = card.dataset.img;
+  const gifSrc = card.dataset.gif;
+  console.log(imgSrc, gifSrc);
+  const main = document.querySelector(".main");
+  main.style.gridTemplateColumns = "1fr";
+  main.innerHTML = `
+    <div class="enlargeCard">
+      <a class="return-btn" type="button"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+      <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
+    </svg></a>
+      <div class="ec-imgBox">
+        <img class="ec-img" src="${gifSrc ? gifSrc : imgSrc}" alt="${title}" />
+      </div>
+      <div class="content">
+        <h3>${title}</h3>
+        <p>${desc}</p>
+        <div class="ec-buttons">
+          <a class="ec-btn" type="button" href="${demo}" target="_blank">DEMO</a>
+          <a class="ec-btn" type="button" href="${github}" target="_blank">GitHub</a>
+        </div>
+      </div>
+    </div>
+  `;
+  document
+    .querySelector(".return-btn")
+    .addEventListener("click", () => location.reload());
+}
+
 document.addEventListener("DOMContentLoaded", createView);
